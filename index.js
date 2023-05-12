c7eea81de5465ea8d3dfaeb2fa5455dae9f48fbd
vicentelustosa <118974637+vicentelustosa@users.noreply.github.com>
2023-05-12 20:19:40 +0000

    correções

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
-// Objeto "postsMap" que mapeia cada post pelo seu id
-let postsMap = {};
+// Criando um objeto Map para armazenar os posts
+const postsMap = new Map();
 
+// Middleware para permitir o uso do req.body para requisições com JSON
 app.use(express.json());
 
-// Retorna todos os posts
+// Rota para listar todos os posts
 app.get('/posts', (req, res) => {
-  const posts = Object.values(postsMap);
-  res.json(posts);
+  // Obtendo um array com todos os valores do objeto Map de posts
+  const posts = Array.from(postsMap.values());
+  res.status(200).json(posts);
 });
 
-// Retorna um post com base no id fornecido
-app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = postsMap[id];
-  if (!post) {
-    res.status(404).send('Post não encontrado');
-  } else {
-    res.json(post);
-  }
-});
-
-// Cria um novo post
+// Rota para criar um novo post
 app.post('/posts', (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  if (!id) {
-    res.status(400).send('O id do post é obrigatório');
-  } else if (postsMap[id]) {
-    res.status(409).send('Já existe um post com o id fornecido');
-  } else {
-    postsMap[id] = post;
-    res.status(201).send(`Post criado com sucesso: ${JSON.stringify(post)}`);
-  }
+  // Criando um novo objeto post com os dados enviados na requisição
+  const post = {
+    id: Date.now().toString(),
+    title: req.body.title,
+    content: req.body.content,
+  };
+
+  // Adicionando o novo post ao objeto Map de posts
+  postsMap.set(post.id, post);
+
+  res.status(201).json(post);
 });
 
-// Atualiza um post existente com base no id fornecido
-app.put('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = req.body;
-  if (!id) {
-    res.status(400).send('O id do post é obrigatório');
-  } else if (!postsMap[id]) {
-    res.status(404).send('Post não encontrado');
-  } else {
-    postsMap[id] = post;
-    res.send(`Post atualizado com sucesso: ${JSON.stringify(post)}`);
-  }
-});
+// Rota para obter um post específico por ID
+app.get('/posts/:id', (req, res) => {
+  // Obtendo o post com o ID especificado do objeto Map de posts
+  const post = postsMap.get(req.params.id);
 
-// Remove um post com base no id fornecido
-app.delete('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = postsMap[id];
   if (!post) {
-    res.status(404).send('Post não encontrado');
+    // Se não encontrar um post com o ID especificado, retorna 404
+    res.status(404).send('Post não encontrado.');
   } else {
-    delete postsMap[id];
-    res.send(`Post removido com sucesso: ${JSON.stringify(post)}`);
+    res.status(200).json(post);
   }
 });
 
-// Inicia o servidor
+// Rota para atualizar um post existente
+app.put('/posts/:id', (req, res) => {
+  // Obtendo o post com o ID especificado do objeto Map de posts
+  const post = postsMap.get(req.params.id);
+
+  if (!post) {
+    // Se não encontrar um post com o ID especificado, retorna 404
+    res.status(404).send('Post não encontrado.');
+  } else {
+    // Atualizando os dados do post com os dados enviados na requisição
+    post.title = req.body.title || post.title;
+    post.content = req.body.content || post.content;
+
+    res.status(200).json(post);
+  }
+});
+
+// Rota para excluir um post existente
+app.delete('/posts/:id', (req, res) => {
+  // Excluindo o post com o ID especificado do objeto Map de posts
+  const deleted = postsMap.delete(req.params.id);
+
+  if (!deleted) {
+    // Se não encontrar um post com o ID especificado, retorna 404
+    res.status(404).send('Post não encontrado.');
+  } else {
+    res.status(204).send();
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Server running...`);
 });
